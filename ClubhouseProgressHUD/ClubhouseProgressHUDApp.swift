2a550c41124a5dc596119a5951d30037ef5d06c1
Alex Nagy <therebeloper@gmail.com>
2021-04-21 18:14:23 +0300

    Initial Commit

---
+++ ClubhouseProgressHUD/ClubhouseProgressHUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubhouseProgressHUDApp.swift
+//  ClubhouseProgressHUD
+//
+//  Created by Alex Nagy on 21.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ClubhouseProgressHUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
